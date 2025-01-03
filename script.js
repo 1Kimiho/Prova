7da618934734976f871a39dc9535890d50111730
Kimi <samillyaquila@gmail.com>
2025-01-03 18:53:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let but = document.querySelector('#but')
 let dvd = document.querySelector('#lateral')
+let aq = document.querySelector('#aa')
+let bq = document.querySelector('#bb')
+let bod = document.querySelector('body')
 
 let aberto = false
 
@@ -15,4 +18,17 @@ but.addEventListener('click',()=>{
         but.innerHTML = '&blacktriangleleft;'
         aberto = true
     }
+})
+
+
+aq.addEventListener('click', ()=>{
+    bod.style.setProperty('--corfundo', '#252525')
+    bod.style.setProperty('--corlateral', '#3f3f3f')
+    dvd.style.setProperty('--letras', 'white')
+
+})
+bq.addEventListener('click', ()=>{
+    bod.style.setProperty('--corfundo', '#968c7c')
+    bod.style.setProperty('--corlateral', '#c0bba5')
+    dvd.style.setProperty('--letras','#463f35')
 })
